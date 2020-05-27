45b6c5450177ba0ce04d61c798cab5d5f8ea5a5a
Samuel Blin <samuelblin44@gmail.com>
2020-05-27 12:50:26 +0200

    main-push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'biblio-livres';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyD76Ig1dNS4Dj9cNJ7uqaa8DaFZ2pd_d04',
+      authDomain: 'biblio-livres-13a8a.firebaseapp.com',
+      databaseURL: 'https://biblio-livres-13a8a.firebaseio.com',
+      projectId: 'biblio-livres-13a8a',
+      storageBucket: 'biblio-livres-13a8a.appspot.com',
+      messagingSenderId: '988783325155',
+      appId: '1:988783325155:web:b52db2b76ca1817c957ad6',
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
